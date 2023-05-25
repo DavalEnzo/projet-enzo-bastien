e3d89a711a1a5a1143a64c2177d2bef32f0797f7
DavalEnzo <davalenzo@gmail.com>
2023-05-25 09:30:24 +0200

    1er commit: installation

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,5 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import ErrorPage from "../router/error-page";;
+import ErrorPage from "../router/error-page";
 
 const Routeur = () => {
     return (
