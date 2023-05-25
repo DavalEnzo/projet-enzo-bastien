9dc9677c1fc6f8af14335d49a58cb3f569e11e8f
DavalEnzo <davalenzo@gmail.com>
2023-05-25 11:50:51 +0200

    Ajout du component CrewDetails.js pour afficher les détails d'un membre d'équipage. ajout de css.

--- src/components/CrewCard.js
+++ src/components/CrewCard.js
@@ -1,7 +1,55 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Loader from './loader'
+import { Container } from 'react-bootstrap'
 
 export default function CrewCard() {
-    useEffect(() => {
+    const [crew, setCrew] = useState([])
+    const [loading, setLoading] = useState(true)
 
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = () => {
+        axios.get('https://api.spacexdata.com/v4/crew')
+            .then(res => {
+                setCrew(res.data)
+                setLoading(false)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    if (loading) return <Loader />
+
+    const crewList = crew.map((member) => {
+        return (
+            <>
+                <Container>
+                    <div className='text-center'>
+                        <h1>Equipage</h1>
+                        <p className='fs-2'>Voici la liste des membres d'équipage de Dragon</p>
+                        <h5 className='text-muted'>Cliquez sur la photo d'un membre pour accéder à ses détails</h5>
+                        <div className='card mt-3 mx-auto' key={member.id}>
+                            <Link className='text-decoration-none' to={`${member.id}`}>
+                                <div className='card-content'>
+                                    <h1>{member.name}</h1>
+                                    <img className={'crewImage'} src={member.image} alt={member.name} />
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                </Container>
+            </>
+        )
     })
+
+    return (
+        <>
+            {crewList}
+        </>
+    )
 }
---
+++ src/pages/Crew.js
@@ -0,0 +1,14 @@
+import SpaceXNavbar from '../components/SpaceXNavbar'
+import CrewCard from '../components/CrewCard'
+import { useParams } from 'react-router-dom'
+import CrewDetails from '../components/CrewDetails'
+
+export default function Crew() {
+    const {id} = useParams()
+
+    if(!id) {
+        return (<><SpaceXNavbar /><CrewCard /></>)
+    } else {
+        return (<><SpaceXNavbar /><CrewDetails id={id}/></>)
+    }
+}
